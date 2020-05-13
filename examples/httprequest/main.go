c0f647903e1a1a477b7f354e1e79a2231ad050b6
Scott Finlay <scott.p.finlay@gmail.com>
2020-05-13 17:46:46 +0200

    improving example

--- examples/httprequest/main.go
+++ examples/httprequest/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httptest"
@@ -11,9 +12,15 @@ import (
 )
 
 func main() {
-	// set up a dummy http server which always returns an error
+	// set up a dummy http server which will return an error twice and then a success
+	errCount := 2
 	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		http.Error(rw, "something went wrong", 500)
+		if errCount == 0 {
+			rw.Write([]byte("success!"))
+		} else {
+			http.Error(rw, "something went wrong", 500)
+			errCount--
+		}
 	}))
 	defer server.Close()
 
@@ -24,11 +31,14 @@ func main() {
 	}
 
 	// prepare the business logic
+	var resp *http.Response // here's where we'll put the final result
+
 	onError := func(err error) {
 		log.Printf("Got error: %s\n", err.Error())
 	}
-	action := func() (err error, retriable bool) {
-		resp, err := server.Client().Do(req)
+	action := func() (error, bool) {
+		var err error
+		resp, err = server.Client().Do(req)
 		if err != nil {
 			return err, true
 		}
@@ -45,8 +55,9 @@ func main() {
 		Do(context.Background(), action)
 
 	if err != nil {
-		log.Printf("Do failed with error: %s\n", err)
+		log.Printf("`Do` failed with error: %s\n", err)
 	} else {
-		log.Println("Success!")
+		responseBody, _ := ioutil.ReadAll(resp.Body)
+		log.Println("Got success response: ", string(responseBody))
 	}
 }
